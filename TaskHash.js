b6df4e0b3916c648b6c437f475e84dae22e97d4f
ictsaji <62506244+ictsaji@users.noreply.github.com>
2024-05-13 13:24:59 +0530

    Add files via upload
    first upload

---
+++ TaskHash.js
@@ -0,0 +1,18 @@
+// trial code for checking the js functionality
+// function changeColor() {
+//     let tex = document.getElementById('texting');
+//     tex.style.color = "red";
+// }
+
+// Project code starts here, Good Luck
+
+
+ 
+ function linkAboutUs() {
+    alert( "Welcome...! You are directed to the link..'ABOUT US'!!")
+}
+
+
+function linkContactUs() {
+    alert( "Welcome...! You are directed to the link..'CONTACT US'!!")
+}
